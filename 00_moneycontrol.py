9f5432c677a0ac5e0c974a6726ec3511303b67bc
sood <sood@sfu.ca>
2016-03-28 02:56:27 -0700

    Adds code to scrape useful data such as Riskometer Rating (Risk Classification), relative and absolute returns and benchmark information for each scheme

--- 00_moneycontrol.py
+++ 00_moneycontrol.py
@@ -14,9 +14,9 @@ import sys
 # sc = SparkContext(conf=conf)
 # sqlContext = SQLContext(sc)
 
-# Get 10 mutual fund families with the highest Assets under Management from Money Control
 money_control_root = 'http://www.moneycontrol.com'
 
+# Get 10 mutual fund families with the highest Assets under Management from Money Control
 markup = requests.get(money_control_root + '/mutual-funds/amc-assets-monitor').text
 
 # make the soup
@@ -83,26 +83,124 @@ for fund in fund_families:
             scheme_aum = u'None'
 
         fscheme_dict    = {
-            'fund_family_name'  : fund['fund_family_name'],
-            'fund_family_url'   : fund['fund_family_url' ],
-            'fund_family_aum'   : fund['fund_family_aum' ],
-            'fund_family_shortcode' fund['fund_family_shortcode'],
-            'scheme_name'       : unicode( data_elems[0].a.string ),
-            'scheme_url'        : money_control_root + data_elems[0].a.attrs['href'],
-            'crisil_rating'     : unicode( data_elems[1].a.string ),
-            'category'          : category_name,
-            'category_url'      : category_url,
-            'latest_nav'        : unicode( data_elems[3].string ),
-            '1yr_return'        : unicode( data_elems[4].string ),
-            'scheme_aum'        : scheme_aum
+            'fund_family_name'      : fund['fund_family_name'],
+            'fund_family_url'       : fund['fund_family_url' ],
+            'fund_family_aum'       : fund['fund_family_aum' ],
+            'fund_family_shortcode' : fund['fund_family_shortcode'],
+            'scheme_name'           : unicode( data_elems[0].a.string ),
+            'scheme_url'            : money_control_root + data_elems[0].a.attrs['href'],
+            'crisil_rating'         : unicode( data_elems[1].a.string ),
+            'category'              : category_name,
+            'category_url'          : category_url,
+            'latest_nav'            : unicode( data_elems[3].string ),
+            '1yr_return'            : unicode( data_elems[4].string ),
+            'scheme_aum'            : scheme_aum
         }
 
         fund_schemes.append( fscheme_dict )
+
+
+# Scrape useful data such as Riskometer Rating (Risk Classification),
+# relative and absolute returns and benchmark information for each scheme
+for idx, scheme in enumerate(fund_schemes):
+    # Read the page at the URL for each scheme
+    markup = requests.get( scheme['scheme_url'] ).text
+    soup = BeautifulSoup(markup, "lxml")
+
+
+    # Riskometer (Risk Rating)
+    scheme['scheme_risk_text'] = unicode(soup.select('.header .MT10 .toplft_cl3 p.avgbgtit')[0].string )
+
+    # Scheme Plan and Scheme Option
+    scheme_plan_option_data    = [unicode( x.string ).strip() for x in soup.select('#planname_frm .FL span')]
     
-
-print( len( fund_schemes ) )
-
-
+    [scheme['scheme_plan'],
+     scheme['scheme_option'] ] = scheme_plan_option_data if scheme_plan_option_data else [u'None', u'None']
 
 
-# ^\s*$\n replace all empty lines
+    # From the Investment Info section, collect scheme fund type,
+    # benchmark name, minimum investment required for this scheme,
+    # last dividend or bonus, if paid else NA
+    sub_soup = soup.select('.mainCont .tog_cont .MT20 .FL td')
+    [scheme['scheme_fund_type'],
+     scheme['scheme_benchmark'],
+     scheme['scheme_min_investment'],
+     scheme['scheme_last_dividend'],
+     scheme['scheme_bonus'] ] = [
+        unicode(x.string).strip() if( x.string and unicode(x.string).strip() != u'N.A.' ) else u'None' for x in [
+            sub_soup[0],
+            sub_soup[3],
+            sub_soup[5],
+            sub_soup[6],
+            sub_soup[7]
+        ]
+    ]
+
+
+
+    # From the performance section, gather
+    # Fund Returns, Category Avg, Difference of Fund Returns and Category Returns
+    # Best of category and worst of category
+    sub_soup = soup.select('.mainCont .tog_cont table')[0]
+
+    # Get the relevant table rows containing this information
+    rows = [row for row in sub_soup if not row.string and unicode(row).strip()][1:]
+
+    for row in rows:
+        row_attrs = [x for x in row.children if unicode(x).strip()]
+
+        row_name  = unicode(row_attrs[0].string).strip().lower()
+
+        # fund returns
+        if row_name == 'fund returns':
+            scheme['fund_ret_1m']      = u'None' if unicode( row_attrs[1].string ) == u'--' else unicode( row_attrs[1].string )
+            scheme['fund_ret_3m']      = u'None' if unicode( row_attrs[2].string ) == u'--' else unicode( row_attrs[2].string )
+            scheme['fund_ret_6m']      = u'None' if unicode( row_attrs[3].string ) == u'--' else unicode( row_attrs[3].string )
+            scheme['fund_ret_1y']      = u'None' if unicode( row_attrs[4].string ) == u'--' else unicode( row_attrs[4].string )
+            scheme['fund_ret_2y']      = u'None' if unicode( row_attrs[5].string ) == u'--' else unicode( row_attrs[5].string )
+            scheme['fund_ret_3y']      = u'None' if unicode( row_attrs[6].string ) == u'--' else unicode( row_attrs[6].string )
+            scheme['fund_ret_5y']      = u'None' if unicode( row_attrs[7].string ) == u'--' else unicode( row_attrs[7].string )
+
+        # category avg
+        if row_name == 'category avg':
+            scheme['cat_avg_ret_1m']   = u'None' if unicode( row_attrs[1].string ) == u'--' else unicode( row_attrs[1].string )
+            scheme['cat_avg_ret_3m']   = u'None' if unicode( row_attrs[2].string ) == u'--' else unicode( row_attrs[2].string )
+            scheme['cat_avg_ret_6m']   = u'None' if unicode( row_attrs[3].string ) == u'--' else unicode( row_attrs[3].string )
+            scheme['cat_avg_ret_1y']   = u'None' if unicode( row_attrs[4].string ) == u'--' else unicode( row_attrs[4].string )
+            scheme['cat_avg_ret_2y']   = u'None' if unicode( row_attrs[5].string ) == u'--' else unicode( row_attrs[5].string )
+            scheme['cat_avg_ret_3y']   = u'None' if unicode( row_attrs[6].string ) == u'--' else unicode( row_attrs[6].string )
+            scheme['cat_avg_ret_5y']   = u'None' if unicode( row_attrs[7].string ) == u'--' else unicode( row_attrs[7].string )
+
+        # difference of fund returns and category returns
+        if row_name == 'difference of fund returns and category returns':
+            scheme['diff_fund_cat_1m'] = u'None' if unicode( row_attrs[1].string ) == u'--' else unicode( row_attrs[1].string )
+            scheme['diff_fund_cat_3m'] = u'None' if unicode( row_attrs[2].string ) == u'--' else unicode( row_attrs[2].string )
+            scheme['diff_fund_cat_6m'] = u'None' if unicode( row_attrs[3].string ) == u'--' else unicode( row_attrs[3].string )
+            scheme['diff_fund_cat_1y'] = u'None' if unicode( row_attrs[4].string ) == u'--' else unicode( row_attrs[4].string )
+            scheme['diff_fund_cat_2y'] = u'None' if unicode( row_attrs[5].string ) == u'--' else unicode( row_attrs[5].string )
+            scheme['diff_fund_cat_3y'] = u'None' if unicode( row_attrs[6].string ) == u'--' else unicode( row_attrs[6].string )
+            scheme['diff_fund_cat_5y'] = u'None' if unicode( row_attrs[7].string ) == u'--' else unicode( row_attrs[7].string )
+
+        # best of category
+        if row_name == 'best of category':
+            scheme['cat_best_1m']      = u'None' if unicode( row_attrs[1].string ) == u'--' else unicode( row_attrs[1].string )
+            scheme['cat_best_3m']      = u'None' if unicode( row_attrs[2].string ) == u'--' else unicode( row_attrs[2].string )
+            scheme['cat_best_6m']      = u'None' if unicode( row_attrs[3].string ) == u'--' else unicode( row_attrs[3].string )
+            scheme['cat_best_1y']      = u'None' if unicode( row_attrs[4].string ) == u'--' else unicode( row_attrs[4].string )
+            scheme['cat_best_2y']      = u'None' if unicode( row_attrs[5].string ) == u'--' else unicode( row_attrs[5].string )
+            scheme['cat_best_3y']      = u'None' if unicode( row_attrs[6].string ) == u'--' else unicode( row_attrs[6].string )
+            scheme['cat_best_5y']      = u'None' if unicode( row_attrs[7].string ) == u'--' else unicode( row_attrs[7].string )
+
+        # worst of category
+        if row_name == 'worst of category':
+            scheme['cat_worst_1m']     = u'None' if unicode( row_attrs[1].string ) == u'--' else unicode( row_attrs[1].string )
+            scheme['cat_worst_3m']     = u'None' if unicode( row_attrs[2].string ) == u'--' else unicode( row_attrs[2].string )
+            scheme['cat_worst_6m']     = u'None' if unicode( row_attrs[3].string ) == u'--' else unicode( row_attrs[3].string )
+            scheme['cat_worst_1y']     = u'None' if unicode( row_attrs[4].string ) == u'--' else unicode( row_attrs[4].string )
+            scheme['cat_worst_2y']     = u'None' if unicode( row_attrs[5].string ) == u'--' else unicode( row_attrs[5].string )
+            scheme['cat_worst_3y']     = u'None' if unicode( row_attrs[6].string ) == u'--' else unicode( row_attrs[6].string )
+            scheme['cat_worst_5y']     = u'None' if unicode( row_attrs[7].string ) == u'--' else unicode( row_attrs[7].string )
+
+    # Print every 100th scheme to verify things are running smoothly
+    if idx % 100 == 0:
+        print( 'Scheme # {0}\n{1}\n\n\n'.format(idx, scheme) )
